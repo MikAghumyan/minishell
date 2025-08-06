1a9d9a9bf0b04ecbf7cc76bbe5b11b5655aeeef5
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2025-08-06 21:00:50 +0400

    initialize main header

---
+++ includes/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maghumya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/06 17:36:47 by maghumya          #+#    #+#             */
+/*   Updated: 2025/08/06 18:00:42 by maghumya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL
+# define MINISHELL
+
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <wait.h>
+#endif
