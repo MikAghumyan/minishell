f640862583797a6232e5c4e65911c31cae069e87
Narek <nsahakyan42@gmail.com>
2025-08-24 16:23:26 +0400

    minor changes

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maghumya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: narek <narek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 17:36:47 by maghumya          #+#    #+#             */
-/*   Updated: 2025/08/09 17:56:49 by maghumya         ###   ########.fr       */
+/*   Updated: 2025/08/24 14:12:00 by narek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ typedef struct s_shell
 	t_token				*tokens;
 }						t_shell;
 
-void					initialize_shell(t_shell *shell, char **envp);
+void    initialize_shell(t_shell *shell, char **envp);
 
 # include "handlers.h"
 # include "tokenizer.h"
