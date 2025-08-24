fe3a0987028f88109ff5d159ab251d4fd447d465
Narek <nsahakyan42@gmail.com>
2025-08-24 14:55:57 +0400

    added ast header file

---
+++ includes/ast.h
@@ -0,0 +1,34 @@
+#ifndef MINISHELL_AST_H
+# define MINISHELL_AST_H
+
+# include "minishell.h"
+# include "tokenizer.h"
+
+typedef enum e_node_type
+{
+    NODE_COMMAND,
+    NODE_PIPE,
+    NODE_AND,
+    NODE_OR,
+    NODE_REDIRECT_IN,
+    NODE_REDIRECT_OUT,
+    NODE_APPEND,
+    NODE_HEREDOC,
+    NODE_SUBSHELL,
+}	e_node_type;
+
+typedef struct s_ast_node
+{
+    e_node_type         type;    // token type
+    char                **args;  // command args
+    char                *redirect_file; // redirection file
+    struct s_ast_node   *left;
+    struct s_ast_node   *right;
+}   t_ast_node;
+
+t_ast_node  *build_ast(t_token *tokens);
+t_ast_node  *create_node(e_node_type type);
+void        free_ast(t_ast_node *node);
+void        print_ast(t_ast_node *node, int depth);
+
+#endif
