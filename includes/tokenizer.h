26aa7d05fb8ac5a5c857f20e46faf33c05d3b8e9
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2025-08-25 15:59:58 +0400

    Refactor tokenizer header for improved readability and consistency

--- includes/tokenizer.h
+++ includes/tokenizer.h
@@ -3,6 +3,8 @@
 
 # include "../libft/libft.h"
 
+typedef struct s_shell	t_shell;
+
 typedef enum e_token_type
 {
 	TOKEN_WORD,
@@ -17,34 +19,34 @@ typedef enum e_token_type
 	TOKEN_RPAREN,
 	TOKEN_OR,
 	TOKEN_AND,
-	TOKEN_INVALID,
-}					e_token_type;
+	TOKEN_INVALID
+}						e_token_type;
 
 typedef struct s_token
 {
-	char			*value;
-	e_token_type	type;
-	struct s_token	*next;
-}					t_token;
+	char				*value;
+	e_token_type		type;
+	struct s_token		*next;
+}						t_token;
 
-t_token				*tokenize_input(t_shell *shell);
-int					process_single_char(t_shell *shell, size_t *i,
-						t_token **tokens);
-size_t				scan_word(const char *input, size_t start,
-						const char end_char);
-int					add_redirect_token(t_token **tokens, const char *input,
-						size_t *i);
-int					add_operator_token(t_token **tokens, const char *input,
-						size_t *i);
-int					add_word_token(t_token **tokens, const char *input,
-						size_t *i);
-int					add_quote_token(t_token **tokens, const char *input,
-						size_t *i);
-int					add_token_slice(t_token **tokens, const char *start,
-						size_t len, e_token_type type);
-void				add_tokens_back(t_token **tokens, t_token *new_token);
-void				free_tokens(t_token *tokens);
-bool				is_operator(const char c);
-void				print_tokens(t_token *tokens);
+int						process_single_char(t_shell *shell, size_t *i,
+							t_token **tokens);
+t_token					*tokenize_input(t_shell *shell);
+size_t					scan_word(const char *input, size_t start,
+							const char end_char);
+int						add_word_token(t_token **tokens, const char *input,
+							size_t *i);
+int						add_redirect_token(t_token **tokens, const char *input,
+							size_t *i);
+int						add_operator_token(t_token **tokens, const char *input,
+							size_t *i);
+int						add_quote_token(t_token **tokens, const char *input,
+							size_t *i);
+bool					is_operator(char c);
+int						add_token_slice(t_token **tokens, const char *start,
+							size_t len, e_token_type type);
+void					add_tokens_back(t_token **tokens, t_token *new_token);
+void					print_tokens(t_token *tokens);
+void					free_tokens(t_token *tokens);
 
 #endif
