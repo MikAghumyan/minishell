f769efc21cc8dcfc2f8f617c7d73a298acca259c
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2025-08-29 15:51:13 +0400

    Add readline suppression file and update initializer and main file headers

--- src/initializer.c
+++ src/initializer.c
@@ -6,7 +6,7 @@
 /*   By: maghumya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 15:38:55 by maghumya          #+#    #+#             */
-/*   Updated: 2025/08/23 15:38:56 by maghumya         ###   ########.fr       */
+/*   Updated: 2025/08/29 14:49:40 by maghumya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@ void	initialize_shell(t_shell *shell, char **envp)
 {
 	shell->env = env_init(envp);
 	// TODO handle error
+	shell->input = NULL;
+	shell->tokens = NULL;
 	shell->exit_status = 0;
 }
