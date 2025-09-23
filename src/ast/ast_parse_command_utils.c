852666baed1cbc9a20f44dfeb737d7dc364d9c0d
Narek <nsahakyan42@gmail.com>
2025-09-23 21:32:58 +0400

    fixed redirect parse, so now we can type arguments after redirect file

--- src/ast/ast_parse_command_utils.c
+++ src/ast/ast_parse_command_utils.c
@@ -55,44 +55,94 @@ t_ast_node  *ast_parse_simple_command(t_token **tokens)
 
 t_ast_node *ast_parse_redirections(t_token **tokens, t_ast_node *cmd_node)
 {
-    t_redirect *redirect;
+    t_redirect  *redirect;
     t_node_type redirect_type;
-    
-    while (*tokens && is_redirect_ast_token(*tokens))
+    char        **new_args;
+    int         current_arg_count;
+    int         additional_args;
+    char        **additional_args_array;
+    int         i;
+
+    current_arg_count = 0;
+    if (cmd_node->args)
     {
-        if ((*tokens)->type == TOKEN_REDIRECT_IN)
-            redirect_type = NODE_REDIRECT_IN;
-        else if ((*tokens)->type == TOKEN_REDIRECT_OUT)
-            redirect_type = NODE_REDIRECT_OUT;
-        else if ((*tokens)->type == TOKEN_APPEND)
-            redirect_type = NODE_APPEND;
-        else if ((*tokens)->type == TOKEN_HEREDOC)
-            redirect_type = NODE_HEREDOC;
-        *tokens = (*tokens)->next;  //skip redirect token
-        if (*tokens && (*tokens)->type == TOKEN_WORD)
+        while (cmd_node->args[current_arg_count])
+            current_arg_count++;
+    }
+    while (*tokens && (is_redirect_ast_token(*tokens) || (*tokens)->type == TOKEN_WORD))
+    {
+        if (is_redirect_ast_token(*tokens))
         {
-            redirect = malloc(sizeof(t_redirect));
-            if (!redirect)
-            {
-                ft_putstr_fd("minishell: memory allocation error\n", 2);
-                return (cmd_node);
-            }
-            redirect->type = redirect_type;
-            redirect->filename = ft_strdup((*tokens)->value);
-            if (!redirect->filename)
-            {
-                free(redirect);
-                ft_putstr_fd("minishell: memory allocation error\n", 2);
-                return (cmd_node);
-            }
-            redirect->next = NULL;
-            add_redirect_to_end(&cmd_node->redirect_files, redirect);
+            if ((*tokens)->type == TOKEN_REDIRECT_IN)
+                redirect_type = NODE_REDIRECT_IN;
+            else if ((*tokens)->type == TOKEN_REDIRECT_OUT)
+                redirect_type = NODE_REDIRECT_OUT;
+            else if ((*tokens)->type == TOKEN_APPEND)
+                redirect_type = NODE_APPEND;
+            else if ((*tokens)->type == TOKEN_HEREDOC)
+                redirect_type = NODE_HEREDOC;
             *tokens = (*tokens)->next;
+            if (*tokens && (*tokens)->type == TOKEN_WORD)
+            {
+                redirect = malloc(sizeof(t_redirect));
+                if (!redirect)
+                {
+                    ft_putstr_fd("minishell: memory allocation error\n", 2);
+                    return (cmd_node);
+                }
+                redirect->type = redirect_type;
+                redirect->filename = ft_strdup((*tokens)->value);
+                if (!redirect->filename)
+                {
+                    free(redirect);
+                    ft_putstr_fd("minishell: memory allocation error\n", 2);
+                    return (cmd_node);
+                }
+                redirect->next = NULL;
+                add_redirect_to_end(&cmd_node->redirect_files, redirect);
+                *tokens = (*tokens)->next;
+            }
+            else
+            {
+                ft_putstr_fd("minishell: syntax error\n", 2);
+                break;
+            }
         }
-        else
+        else if ((*tokens)->type == TOKEN_WORD)
         {
-            ft_putstr_fd("minishell: syntax error near redirect\n", 2);
-            break;
+            additional_args_array = collect_ast_arguments(tokens, &additional_args);
+            if (additional_args > 0)
+            {
+                new_args = malloc(sizeof(char*) * (current_arg_count + additional_args + 1));
+                if (!new_args)
+                {
+                    i = 0;
+                    while (i < additional_args)
+                    {
+                        free(additional_args_array[i]);
+                        i++;
+                    }
+                    free(additional_args_array);
+                    return (cmd_node);
+                }
+                i = 0;
+                while (i < current_arg_count)
+                {
+                    new_args[i] = cmd_node->args[i];
+                    i++;
+                }
+                i = 0;
+                while (i < additional_args)
+                {
+                    new_args[current_arg_count + i] = additional_args_array[i];
+                    i++;
+                }
+                new_args[current_arg_count + additional_args] = NULL;
+                free(cmd_node->args);
+                free(additional_args_array);
+                cmd_node->args = new_args;
+                current_arg_count += additional_args;
+            }
         }
     }
     return (cmd_node);
