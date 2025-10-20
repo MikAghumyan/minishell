5df6d7b6f3c4907d68d2009bbba2ae13eb8b8b8b
Narek <nsahakyan42@gmail.com>
2025-10-20 21:20:43 +0400

    add match_pattern function

--- src/expander/expand_wildcard.c
+++ src/expander/expand_wildcard.c
@@ -13,3 +13,26 @@ static bool has_wildcard(const char *str)
     }
     return (false);
 }
+
+static bool match_pattern(const char *filename, const char *pattern)
+{
+    if (*pattern == '\0')
+        return (*filename == '\0');
+    if (*pattern == '*')
+    {
+        while (*pattern == '*')
+            pattern++;
+        if (*pattern == '\0')
+            return (true);
+        while (*filename)
+        {
+            if (match_pattern(filename, pattern))
+                return (true);
+            filename++;
+        }
+        return (false);
+    }
+    if (*pattern == *filename)
+        return (match_pattern(filename + 1, pattern + 1));
+    return (false);
+}
