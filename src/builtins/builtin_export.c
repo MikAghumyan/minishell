a131fa5fac8899e8179f820f718e29ef88ecb420
Mikayel Aghumyan <mikaelaghumyan@outlook.com>
2025-10-21 17:19:07 +0400

    Fix: check correct identifier in export builtin

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -12,10 +12,32 @@
 
 #include "../../includes/builtins.h"
 
+static bool	is_invalid_identifier(const char *args)
+{
+	const char	*assign;
+
+	assign = ft_strchr(args, '=');
+	if (assign == args)
+		return (true);
+	if (!assign)
+		assign = args + ft_strlen((char *)args);
+	if (args[0] && !ft_isalpha(args[0]) && args[0] != '_')
+		return (true);
+	while (args && args < assign)
+	{
+		if (*args == '+' && *(args + 1) == '=')
+			break ;
+		if (!ft_isalnum(*args) && *args != '_')
+			return (true);
+		args++;
+	}
+	return (false);
+}
+
 int	builtin_export(const char **args, t_shell *shell)
 {
 	size_t	i;
-	char	*assign;
+	int		is_invalid;
 
 	if (!args[1])
 		return (builtin_env(args, shell));
@@ -23,16 +45,15 @@ int	builtin_export(const char **args, t_shell *shell)
 	i = 0;
 	while (args[++i])
 	{
-		assign = ft_strchr(args[i], '=');
-		if (assign == NULL)
-			continue ;
-		if (assign == args[i])
+		is_invalid = is_invalid_identifier(args[i]);
+		if (is_invalid)
 		{
 			ft_fprintf(STDERR_FILENO,
 				"minishell: export: `%s': not a valid identifier\n", args[i]);
 			shell->exit_status = 1;
-			continue ;
 		}
+		if (is_invalid || !ft_strchr(args[i], '='))
+			continue ;
 		env_set(shell->env, args[i], NULL);
 	}
 	return (shell->exit_status);
